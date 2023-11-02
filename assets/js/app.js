9b53a3ff4b7d10b34745c28b1e2b0c95bad1d9d1
PhoenixxDevs <john-phoenix@outlook.com>
2023-11-02 13:38:05 +0000

    feat initialisation and animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
 const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
+export const ctx = canvas.getContext('2d');
 
+export let WIDTH, HEIGHT;
+export let WIDTH_HALF, HEIGHT_HALF;
+
+function init(){
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+  WIDTH_HALF = WIDTH / 2;
+  HEIGHT_HALF = HEIGHT / 2;
+
+  animate();
+}
+
+function animate(){
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  window.requestAnimationFrame(animate);
+}
