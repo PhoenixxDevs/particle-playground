9ed457df412eb74595a9b266d8fcf4d1027c84fd
PhoenixxDevs <john-phoenix@outlook.com>
2023-11-06 02:48:12 +0000

    feat add sparks on mouse move

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,16 @@
 import { particleCreate, particlesUpdate } from "./particle.js";
 
 const canvas = document.getElementById("canvas");
-export const ctx = canvas.getContext('2d');
-const buttons = document.getElementById('container');
-const startButton = document.getElementById('go');
-const clear = document.getElementById('clear');
+export const ctx = canvas.getContext("2d");
+const buttons = document.getElementById("container");
+const startButton = document.getElementById("go");
+const clear = document.getElementById("clear");
+
+export const mouse = {
+  pos: {
+    x: 0, y: 0
+  }
+}
 
 export let WIDTH, HEIGHT;
 export let WIDTH_HALF, HEIGHT_HALF;
@@ -13,16 +19,16 @@ export let particles = [];
 let mode = null;
 let running = false;
 
-function pickMode(){
-  for(let i=0; i<buttons.children.length; i++) {
-    if(buttons.children[i].classList.length > 2){
-      mode = buttons.children[i].id;
-      return mode;
-    }
-  }
-}
+// function pickMode() {
+//   for (let i = 0; i < buttons.children.length; i++) {
+//     if (buttons.children[i].classList.length > 2) {
+//       mode = buttons.children[i].id;
+//       return mode;
+//     }
+//   }
+// }
 
-function init(){
+function init() {
   mode = null;
   WIDTH = window.innerWidth;
   HEIGHT = window.innerHeight;
@@ -31,27 +37,63 @@ function init(){
 
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
-
-  console.log(pickMode())
-
-  particleCreate( pickMode(), 30, false );
-
-  if(!running){running = true; animate();}
 }
 
-function stop(){
+function start() {
+  particleCreate(mode, 30);
+
+  if (!running) {
+    running = true;
+    animate();
+  }
+}
+
+function stop() {
   particles = [];
-
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
   running = false;
+  for(let i = 0; i < particles.length; i++) {
+    particles.pop();
+  }
 }
 
-function animate(){
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  if(!running){return}
+function animate() {
+  ctx.fillStyle = 'rgba(0,0,0,0.3)';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  if (!running) {
+    return;
+  }
   particlesUpdate();
 
   window.requestAnimationFrame(animate);
 }
 
-go.addEventListener('click', init);
-clear.addEventListener('click', stop);
+startButton.addEventListener("click", start);
+clear.addEventListener("click", stop);
+buttons.children[0].addEventListener("click",() => {
+  mode = 'default'; 
+})
+buttons.children[1].addEventListener("click",() => {
+  mode = 'bubble';  console.log(mode)
+})
+buttons.children[2].addEventListener("click",() => {
+  mode = 'mouseSpark';
+  if(!running){
+    running = true;
+    animate();
+  }
+
+})
+buttons.children[3].addEventListener("click",() => {
+  mode = 'default';
+})
+window.addEventListener('mousemove', (e) => {
+  mouse.pos.x = e.pageX;
+  mouse.pos.y = e.pageY;
+
+  if(mode === 'mouseSpark' && running) {
+    particleCreate(mode, 1);
+  }
+});
+window.addEventListener('resize', init);
+window.addEventListener('DOMContentLoaded', init);
--- assets/js/particle.js
+++ assets/js/particle.js
@@ -1,4 +1,12 @@
-import { ctx, WIDTH, HEIGHT, WIDTH_HALF, HEIGHT_HALF, particles } from './app.js';
+import {
+  ctx,
+  WIDTH,
+  HEIGHT,
+  WIDTH_HALF,
+  HEIGHT_HALF,
+  particles,
+  mouse,
+} from "./app.js";
 
 let particlesLength = 0;
 
@@ -6,12 +14,15 @@ class Particle {
   constructor(config) {
     this.type = config.type;
     this.size = config.size;
+    this.weight = config.weight;
     this.pos = config.pos;
     this.vel = config.vel;
     this.color = config.color;
+    this.alpha = config.alpha;
     this.remove = false;
 
     this.sizeModifier = config.sizeModifier;
+    this.alphaModifier = config.alphaModifier;
   }
   draw() {
     ctx.fillStyle = this.color;
@@ -21,62 +32,83 @@ class Particle {
   }
 
   // PARTICLE CONTROL METHODS
-  move(){
+  move() {
     this.pos.x += this.vel.x;
     this.pos.y += this.vel.y;
   }
-  contain(){
-    if(this.pos.x < this.size){
+  gravity(){
+    this.vel.y += this.weight;
+  }
+  contain() {
+    if (this.pos.x < this.size) {
       this.pos.x = this.size;
       this.vel.x *= -1;
-    }
-    else if (this.pos.x > WIDTH - this.size){
+    } else if (this.pos.x > WIDTH - this.size) {
       this.pos.x = WIDTH - this.size;
       this.vel.x *= -1;
     }
-    if(this.pos.y < this.size){
+    if (this.pos.y < this.size) {
       this.pos.y = this.size;
       this.vel.y *= -1;
-    }
-    else if (this.pos.y > HEIGHT - this.size){
+    } else if (this.pos.y > HEIGHT - this.size) {
       this.pos.y = HEIGHT - this.size;
       this.vel.y *= -1;
     }
   }
-  sizeMod(){
+  sizeMod() {
     this.size += this.sizeModifier;
+    if(this.size < 0.1){this.size = 0.1}
   }
-  removeOffScreen(){
-    if( this.pos.x < -this.size ||
+  alphaMod() {
+    this.alpha += this.alphaModifier;
+  }
+  removeOffScreen() {
+    if (
+      this.pos.x < -this.size ||
       this.pos.x > WIDTH + this.size ||
       this.pos.y < -this.size ||
-      this.pos.y > HEIGHT + this.size){ 
-        this.remove = true;
+      this.pos.y > HEIGHT + this.size
+    ) {
+      this.remove = true;
+    }
+  }
+  removeSmallParticles() {
+    if (this.size <= 0.1) {
+      this.remove = true;
     }
   }
 
   // TYPE FUNCTIONALITY
-  default(){
+  default() {
     this.move();
     this.contain();
   }
-  bubble(){
+  bubble() {
     this.move();
     this.sizeMod();
     this.removeOffScreen();
   }
-
+  mouseSpark() {
+    this.gravity();
+    this.move();
+    this.alphaMod();
+    this.sizeMod();
+    this.color = `hsla(${Math.random() * 13 + 36}, 93%, 65%, ${this.alpha})`;
+  }
 
   update() {
-
-    switch(this.type) {
-      case 'default':
+    switch (this.type) {
+      case "default":
         this.default();
-      break;
-      
-      case 'bubble':
+        break;
+
+      case "bubble":
         this.bubble();
-      break;
+        break;
+
+      case "mouseSpark":
+        this.mouseSpark();
+        break;
     }
 
     this.draw();
@@ -85,79 +117,111 @@ class Particle {
 
 // CREATION FUNCTION
 
-
-export function particleCreate(type, amount){
+export function particleCreate(type, amount) {
   let config = {};
   let size = 0;
+  let alpha = 0;
 
-  for(let i = 0; i < amount; i++){
-    switch(type){
-
+  for (let i = 0; i < amount; i++) {
+    switch (type) {
       // DEFAULT PARTICLE DEFINITIONS
-      case 'default': default: 
+      case "default":
+      default:
         size = Math.floor(Math.random() * 10) + 4;
         config = {
-          type: 'default',
+          type: "default",
           size: size,
           pos: {
-            x: Math.floor(
-              (size) + (Math.random() * WIDTH) - (size * 2)
-            ),
-            y: Math.floor(
-              (size) + (Math.random() * HEIGHT) - (size * 2)
-            )
+            x: Math.floor(size + Math.random() * WIDTH - size * 2),
+            y: Math.floor(size + Math.random() * HEIGHT - size * 2),
           },
           vel: {
-            x: ((100 * Math.floor(Math.random() * 4) - 2)) / 100,
-            y: ((100 * Math.floor(Math.random() * 4) - 2)) / 100
+            x: (100 * Math.floor(Math.random() * 4) - 2) / 100,
+            y: (100 * Math.floor(Math.random() * 4) - 2) / 100,
           },
           // color: 'white'
-          color: `hsl(${Math.floor(Math.random() * 80 + 230)}, 70%, 70%)`
+          color: `hsl(${Math.floor(Math.random() * 80 + 230)}, 70%, 70%)`,
         };
         //MINIMUM PARTICLE SPEED
-        if(config.vel.x < 0.2 && config.vel.x > 0){ config.vel.x = 0.2; }
-        if(config.vel.x > -0.2 && config.vel.x < 0){ config.vel.x = -0.2; }
-        if(config.vel.y < 0.2 && config.vel.y > 0){ config.vel.y = 0.2; }
-        if(config.vel.y > -0.2 && config.vel.y < 0){ config.vel.y = -0.2; }
-      break;
+        if (config.vel.x < 0.2 && config.vel.x > 0) {
+          config.vel.x = 0.2;
+        }
+        if (config.vel.x > -0.2 && config.vel.x < 0) {
+          config.vel.x = -0.2;
+        }
+        if (config.vel.y < 0.2 && config.vel.y > 0) {
+          config.vel.y = 0.2;
+        }
+        if (config.vel.y > -0.2 && config.vel.y < 0) {
+          config.vel.y = -0.2;
+        }
+        break;
 
-      case 'bubble':
+      //BUBBLE DEFINITIONS
+      case "bubble":
         config = {
-          type: 'bubble',
+          type: "bubble",
           size: Math.floor(Math.random() * 3),
-          pos: {x: WIDTH_HALF, y: HEIGHT_HALF},
+          pos: { x: WIDTH_HALF, y: HEIGHT_HALF },
           vel: {
-            x: ((100 * Math.floor(Math.random() * 8))) / 100 - 4,
-            y: ((100 * Math.floor(Math.random() * 8))) / 100 - 4
+            x: (100 * Math.floor(Math.random() * 8)) / 100 - 4,
+            y: (100 * Math.floor(Math.random() * 8)) / 100 - 4,
           },
           // color: 'white'
-          color: `hsla(${Math.floor(Math.random() * 40 + 170)}, 70%, ${Math.floor(Math.random() * 15) + 70}%, 0.65)`,
-          sizeModifier: Math.random() * 0.5 + 0.5
+          color: `hsla(${Math.floor(Math.random() * 40 + 170)}, 70%, ${
+            Math.floor(Math.random() * 15) + 70
+          }%, 0.65)`,
+          sizeModifier: Math.random() * 0.5 + 0.5,
         };
         //MINIMUM PARTICLE SPEED
-        if(config.vel.x < 0.3 && config.vel.x > 0){ config.vel.x = 0.3; }
-        if(config.vel.x > -0.3 && config.vel.x < 0){ config.vel.x = -0.3; }
-        if(config.vel.y < 0.3 && config.vel.y > 0){ config.vel.y = 0.3; }
-        if(config.vel.y > -0.3 && config.vel.y < 0){ config.vel.y = -0.3; 
-      break;
-      }
+        if (config.vel.x < 0.3 && config.vel.x > 0) {
+          config.vel.x = 0.3;
+        }
+        if (config.vel.x > -0.3 && config.vel.x < 0) {
+          config.vel.x = -0.3;
+        }
+        if (config.vel.y < 0.3 && config.vel.y > 0) {
+          config.vel.y = 0.3;
+        }
+        if (config.vel.y > -0.3 && config.vel.y < 0) {
+          config.vel.y = -0.3;
+        }
+        break;
+
+      //MOUSESPARKS DEFINITIONS
+      case "mouseSpark":
+        alpha = Math.random() * 0.2 + 0.8;
+        size = Math.random() * 3 + 1;
+        config = {
+          type: "mouseSpark",
+          size: size,
+          weight: size / 25,
+          pos: { x: mouse.pos.x, y: mouse.pos.y },
+          vel: {
+            x: Math.random() * 10 - 5,
+            y: Math.random() * 5 - 1,
+          },
+          color: `hsla(${Math.random() * 17 + 32}, 93%, 67%, ${alpha})`,
+          alpha: alpha,
+          sizeModifier: Math.random() * -0.003,
+          alphaModifier: Math.random() * -0.23,
+        };
+        break;
     }
 
     particles.push(new Particle(config));
   }
 }
 
-
 // UPDATE FUNCION
 
-
-export function particlesUpdate(){
+export function particlesUpdate() {
   particlesLength = particles.length;
-  for(let i = 0; i < particlesLength; i++){
+  for (let i = 0; i < particlesLength; i++) {
     particles[i].update();
-    if(particles[i].remove){
+    if (particles[i].remove) {
       particles.splice(i, 1);
       particlesLength = particles.length;
     }
   }
-};
+}
